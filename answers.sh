8ca5f1661eb89aabda47ecb17ea869052ca3c618
rahhal ali <alih.rahhal@hotmail.com>
2021-06-02 17:11:01 +0300

    update the answe,sh

--- answers.sh
+++ answers.sh
@@ -5,3 +5,15 @@ SELECT Points FROM students WHERE name="Alex";
 INSERT INTO students (name,Age,Gender,Points) VALUES ("Ali",25,"M",2021);
 UPDATE students SET Points = Points + 100 WHERE name="Basma";
  UPDATE students SET Points = (Points -100) WHERE name="Alex"; 
+ 
+
+INSERT INTO graduates
+SELECT * FROM students
+WHERE name="Layal";
+
+ALTER TABLE graduates
+ADD graduation text;
+
+UPDATE INTO graduates graduation ="08-09-2018";
+
+DELETE FROM students WHERE name="Layal";
